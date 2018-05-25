75a3fe7c91ead98acedc8df8733aea9bb8394144
Chandler Davis <chandlerfdavis@gmail.com>
2018-05-25 13:47:31 -0500

    Repair the food plugin and prepare for mpc capability
    - Food plugin now uses a lambda command to filter
    - Added a call to install mpc to the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 RUN echo http://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
 RUN apk update
 RUN apk add --update python3 python3-dev vim bash    \
-                curl build-base libffi-dev openssl-dev    \
+                curl build-base libffi-dev openssl-dev mpc   \
     && curl "https://bootstrap.pypa.io/get-pip.py" | python3 \
     && pip install --upgrade pip setuptools     \
     && ln -s /usr/bin/python3 /usr/bin/python   \
@@ -17,5 +17,6 @@ WORKDIR /app
 RUN pip install -e .
 RUN pip install -r requirements.txt --no-compile
 CMD /app/bin/limbo
+EXPOSE 6680
 
 # vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
--- limbo/plugins/food.py
+++ limbo/plugins/food.py
@@ -2,26 +2,20 @@
 
 import random
 import re
-from datetime import datetime
+from datetime import datetime, time
+
+is_open = lambda x: x[1] < datetime.now().time()
+
+foodList = ("Subway", time(7, 00)), ("Jimmy Johns", time(10, 30)), ("Mr. Burrito", time(11, 00)), ("Panda Express", time(10, 00)), ("Potbelly", time(9, 00)), ("Blaze", time(10, 30)), ("Smokin' Oaks", time(11, 00)), ("Fuzzy's Tacos", time(11, 00)), ("Pizza Pit", time(11, 00)), ("Fighting Burrito", time(11, 00)), ("Thai Kitchen", time(11, 00)), ("Freddy's", time(10, 30))
 
 # (<Name>, (<Hour>, <Minute>))
-foodList = ("Subway", (7, 00)), ("Jimmy Johns", (10, 30)), ("Mr. Burrito", (11, 00)), ("Panda Express", (10, 00)), ("Potbelly", (9, 00)), ("Blaze", (10, 30)), ("Smokin' Oaks", (11, 00)), ("Fuzzy's", (10, 00)), ("Pizza Pit", (11, 00)), ("Dominos", (10, 30)), ("Jeff's", (10, 30)), ("Fighting Burrito", (11, 00)), ("Thai Kitchen", (11, 00))
 def decide_food():
 	random.seed(datetime.now().microsecond)
-	isOpen = False
-	while(not isOpen):
-		choice = random.choice(foodList)
+	hour = datetime.now().hour
+	minute = datetime.now().minute
+	temp = list(filter(is_open, foodList))
 
-		if choice[1][0] < datetime.now().hour:
-			continue
-		elif choice[1][0] > datetime.now().hour:		
-			isOpen = True 
-			break
-		elif choice[1][1] > datetime.now().minute:
-			isOpen = True
-			break
-
-	return "You should eat at " + choice[0]
+	return "You should eat at {0}".format(random.choice(temp)[0])
 
 def on_message(msg, server):
     text = msg.get("text", "")
@@ -29,7 +23,7 @@ def on_message(msg, server):
     if not match:
         return
 
-    return "This is not working currently"
+    return decide_food()
 
 
 on_bot_message = on_message
